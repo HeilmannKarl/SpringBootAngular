a48b1e09d709fc2d4b66b8dcf2d8d975c7bd6f97
Karl Heilmann <karl.heilmann@shinefour.de>
2022-11-13 23:29:59 +0100

    jackson

--- src/main/java/de/heilmann/springboot/angular/template/domain/User.java
+++ src/main/java/de/heilmann/springboot/angular/template/domain/User.java
@@ -1,12 +1,18 @@
 package de.heilmann.springboot.angular.template.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.NoArgsConstructor;
+import lombok.extern.jackson.Jacksonized;
 
 import javax.persistence.*;
 
 @Entity
-@Table(name = "user_tabel")
+@Table(name = "user_table")
+@Jacksonized
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
