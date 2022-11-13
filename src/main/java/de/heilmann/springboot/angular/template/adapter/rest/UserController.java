bfd12156ee3614743c99d8f27306226cb7d81d9e
Karl Heilmann <kh@karl-heilmann.info>
2022-11-13 22:35:19 +0100

    Entity und Controller added

--- src/main/java/de/heilmann/springboot/angular/template/adapter/rest/UserController.java
+++ src/main/java/de/heilmann/springboot/angular/template/adapter/rest/UserController.java
@@ -3,6 +3,8 @@ package de.heilmann.springboot.angular.template.adapter.rest;
 import de.heilmann.springboot.angular.template.application.UserRepository;
 import de.heilmann.springboot.angular.template.domain.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,7 +16,9 @@ public class UserController {
 
     private final UserRepository userRepository;
 
-    @GetMapping("/users")
+    @GetMapping(value = "/users", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
     public List<User> getUsers() {
         return (List<User>) userRepository.findAll();
     }
