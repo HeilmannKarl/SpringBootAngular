aeb9e5784c2fc834a165d55679910957e59ce299
Karl Heilmann <kh@karl-heilmann.info>
2022-11-13 21:01:24 +0100

    Initial commit

---
+++ src/test/java/de/heilmann/springboot/angular/SpringBootAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package de.heilmann.springboot.angular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
